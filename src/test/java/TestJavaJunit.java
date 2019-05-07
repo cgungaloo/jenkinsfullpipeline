1a7af5f0b819b6104eb383f358dc0d9856fc5cb2
cgungaloo <cgungaloo@yahoo.com>
2019-05-07 22:20:59 +0100

    Added test update

--- src/test/java/TestJavaJunit.java
+++ src/test/java/TestJavaJunit.java
@@ -3,6 +3,6 @@ import org.junit.jupiter.api.Test;
 public class TestJavaJunit {
    @Test
    public void TestjustAnExample() {
-    System.out.println("Hello!!!!!");
+    System.out.println("Hello There!!!!!");
    }
 }
